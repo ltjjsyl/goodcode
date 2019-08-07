fcf7f62c2593a9981c6fe6c67a896d4c6901895e
chuxiao <645857537@qq.com>
2019-08-07 21:38:59 +0800

    add:10.md

---
+++ src/main/java/cn/chuxiao/onjava8/polymorphism/PolyConstructors.java
@@ -0,0 +1,38 @@
+package cn.chuxiao.onjava8.polymorphism;
+
+/**
+ * 构造器多态
+ * */
+// Constructors and polymorphism
+// don't produce what you might expect
+class Glyph {
+    void draw() {
+        System.out.println("Glyph.draw()");
+    }
+
+    Glyph() {
+        System.out.println("Glyph() before draw()");
+        draw();//不要这么做
+        System.out.println("Glyph() after draw()");
+    }
+}
+
+class RoundGlyph extends Glyph {
+    private int radius = 1;
+
+    RoundGlyph(int r) {
+        radius = r;
+        System.out.println("RoundGlyph.RoundGlyph(), radius = " + radius);
+    }
+
+    @Override
+    void draw() {
+        System.out.println("RoundGlyph.draw(), radius = " + radius);
+    }
+}
+
+public class PolyConstructors {
+    public static void main(String[] args) {
+        new RoundGlyph(5);
+    }
+}
